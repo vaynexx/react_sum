1edf9c90517f0a53dd1c76dc8bcda80ff83436e2
Vitalii <vitalilkrutoy234@gmail.com>
2026-06-04 16:22:59 +0300

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
 
-export const App = () => {
-  // Компонент Sum оголошуємо тут же
-  const Sum = ({ a = 0, b = 0 }) => {
-    const result = a + b;
-    return (
-      <p>
-        Sum of {a} and {b} is {result}
-      </p>
-    );
-  };
-
-  return (
-    <div>
-      <Sum a={2} b={3} />
-      <Sum a={-5} b={5} />
-      <Sum a={10} />
-      <Sum b={5} />
-      <Sum />
-    </div>
-  );
-};
+export const App = () => (
+  <>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
+    {/* Replace paragraphs with Sum componets */}
+    {/* And remove commented lines :) */}
+  </>
+);
